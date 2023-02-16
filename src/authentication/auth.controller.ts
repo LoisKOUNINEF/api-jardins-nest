d352b2c7cfd3171783c8ea809824fd3f5d91b088
Lois KOUNINEF <lkouninef@gmail.com>
2023-02-16 10:04:00 +0100

    remove unused route

--- src/authentication/auth.controller.ts
+++ src/authentication/auth.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { Admin } from 'src/authorization/admin.decorator';
-import { OwnerId } from 'src/authorization/ownerId.decorator';
 import { AuthenticatedGuard } from './authenticated.guard';
 
 @Controller('auth')
@@ -14,12 +13,6 @@ export class AuthController {
   @Get('admin')
   @Admin(true)
   checkIfAdmin(): boolean {
-    return true;
-  }
-
-  @Get('owner')
-  @OwnerId(true)
-  checkIfOwner(): boolean {
     return true;
   }
 }
